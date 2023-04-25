c02637bea2e69daf5cade8bf958c117ff4518676
Daniela Pourrieux <dhannychocolateteamo@gmail.com>
2023-04-25 15:29:02 -0300

    cambio de video

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,12 @@ import "../styles/home.css";
 import { Container, Row, Col, CardSubtitle } from "reactstrap";
 import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
-import heroVideo from "../assets/images/hero-video.mp4";
+import heroVideo from "../assets/images/hero-video.mkv";
 import worldImg from "../assets/images/world.png";
 import experienceImg from "../assets/images/experience.png";
 
 import Subtitle from "./../shared/subtitle";
-/* import SearchBar from "./../shared/SearchBar"; */
+import SearchBar from "./../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
@@ -56,7 +56,7 @@ const Home = () => {
 							</div>
 						</Col>
 
-						{/* <SearchBar /> */}
+						<SearchBar />
 					</Row>
 				</Container>
 			</section>
